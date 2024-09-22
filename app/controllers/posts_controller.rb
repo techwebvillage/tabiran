81dd89d1080d35722527316193f5c962d5f1369e
ken789tar <wanifuchikentarou@gmail.com>
2024-09-22 17:01:23 +0900

    post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,8 +27,14 @@ class PostsController < ApplicationController
     redirect_to post_path(post.id)
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
   private
   def post_params
-    params.require(:post).permit(:location, :text)
+    params.require(:post).permit(:location, :text, :image)
   end
 end
