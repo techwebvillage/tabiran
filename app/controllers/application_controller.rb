633d651a3b1a6d6814d1a6689b9111ce4303f402
ken789tar <wanifuchikentaraou@gmail.com>
2024-09-24 09:38:55 +0900

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
-    class ApplicationController < ActionController::Base
 
         before_action :configure_permitted_parameters, if: :devise_controller?
     
         protected
         def configure_permitted_parameters
           devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-        end  
-    end
+        end 
+        
+        def after_sign_in_path_for(resouece)
+            posts_path
+        end
 end
