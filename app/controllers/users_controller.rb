c0ef04dec17460f3d4bf5b82e76dc016ebb581ca
ken789tar <174225250+ken789tar@users.noreply.github.com>
2024-09-29 18:02:22 +0900

    user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :profile_image)
+    params.require(:user).permit(:name, :email, :profile_image, :profile)
   end
 end
