34958b5eebf4ecb28cba47ad1726e071ad4dc24a
ken789tar <174225250+ken789tar@users.noreply.github.com>
2024-09-30 12:51:12 +0900

    follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,16 @@ class User < ApplicationRecord
   has_many :follower_user, through: :followed, source: :follower
 
   attachment :profile_image
+
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  def following?
+    following_user.include?(user)
+  end
 end
