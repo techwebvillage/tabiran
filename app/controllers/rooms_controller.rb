468d0d94495f59929b983617cb0320bdddf8a1cd
ken789tar <174225250+ken789tar@users.noreply.github.com>
2024-10-12 08:38:57 +0900

    チャット機能

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,8 @@
 class RoomsController < ApplicationController
+  def create
+    @room = Room.create(user_id: current_user.id)
+    @current_entry = Entry.create(user_id: current_user.id, room_id: @room.id)
+    @another_entry = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
+    redirect_to room_path(@room)
+  end
 end
