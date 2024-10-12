cbca13548188f8d6c45ecb69d3c479199a02d2fd
ken789tar <174225250+ken789tar@users.noreply.github.com>
2024-10-12 12:33:23 +0900

    チャット機能

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,4 +5,11 @@ class RoomsController < ApplicationController
     @another_entry = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
     redirect_to room_path(@room)
   end
+
+  def show
+    @room = Room.find(params[:id])
+    if Entry.where(user_id: current_user.id, room_id: @room.id).present?
+      @messages = @room.messages
+      @messege = Message.new
+      @entries = @room.entries
 end
