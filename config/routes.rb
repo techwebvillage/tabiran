5fba02030dc0b03a6f5e43d3486362c9e8d80e4a
ken789tar <174225250+ken789tar@users.noreply.github.com>
2024-10-18 20:42:33 +0900

    評価

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   root :to => 'homes#top'
 
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  # devise_scope :user do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
 end
