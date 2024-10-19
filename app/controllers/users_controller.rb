a85cb283f75da54cfca86056b2d79341a41ef833
ken789tar <174225250+ken789tar@users.noreply.github.com>
2024-10-19 12:04:23 +0900

    userpeage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.page(params[:page]).per(8).reverse_order
+    @posts = @user.posts.published.page(params[:page]).per(8).reverse_order
     @following_users = @user.following_user
     @follower_users = @user.follower_user
     @current_entry = Entry.where(user_id: current_user.id)
